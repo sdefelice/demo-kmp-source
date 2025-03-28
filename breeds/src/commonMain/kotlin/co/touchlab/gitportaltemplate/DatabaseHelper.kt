a231d8bd089809e3a05a9325d5d4ac076fcb10f3
sdefelice <120423988+sdefelice@users.noreply.github.com>
2025-03-28 16:41:19 -0400

    Merge pull request #4 from sdefelice/feature-branch-1

--- breeds/src/commonMain/kotlin/co/touchlab/gitportaltemplate/DatabaseHelper.kt
+++ breeds/src/commonMain/kotlin/co/touchlab/gitportaltemplate/DatabaseHelper.kt
@@ -12,6 +12,9 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOn
 
+/**
+ * Hello from Android!
+ */
 internal class DatabaseHelper(
     sqlDriver: SqlDriver,
     private val breedAnalytics: BreedAnalytics,
